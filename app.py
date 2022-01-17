6ed0ed265e5d707924d976ed92dd58ff3fe5b3a0
rammafitra <rammafitracyt@gmail.com>
2022-01-17 19:24:32 +0700

    Jan-17-2022, menambahkan client side dan integrasi

---
+++ app.py
@@ -0,0 +1,25 @@
+from crypt import methods
+from urllib import response
+from flask import Flask, render_template, request, jsonify 
+from flask_cors import CORS 
+
+from chat import get_response 
+
+app = Flask(__name__)
+CORS(app)
+
+# @app.route("/", methods=["GET"])
+# def index_get():
+#     return render_template("base.html")
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    text = request.get_json().get("message")
+    response = get_response(text)
+    message = {"answer": response}
+    return jsonify(message)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
